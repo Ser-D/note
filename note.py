40146858473495cf58e9c8a2fa1ae38139037d0a
Ser <142014713+Ser-D@users.noreply.github.com>
2023-12-16 19:14:45 +0200

    Add files via upload

--- note.py
+++ note.py
@@ -8,6 +8,9 @@ class Note:
 
     def add_teg(self, teg):
         self.teg.append(teg)
+
+    def change_note(self, new_text):
+        self.note = new_text
     
     def __str__(self):
         return f"Note name: {self.name}, tegs: {'; '.join(p for p in self.teg)}, note: {self.note}"
@@ -17,23 +20,55 @@ class Notebook(UserDict):
         if isinstance(note, Note):
             self.data[note.name] = note
 
+    def find_note(self, name):
+        for note in self.data.values():
+            if name in note.name:
+                return note
+    
+    def find_note_teg(self, teg):
+        res = None
+        res = []
+        for note in self.data.values():
+            if teg in note.teg:
+                res.append(note)
+        return res
+
 notebook = Notebook()
+
+# Екземпляр класу Note
 cleans = Note('прибрати кімнату', 'віник,  вода і інше')
 cleans.add_teg('необовязкове')
 cleans.add_teg('щоденне')
 
 notebook.add_note(cleans)
 
-print('-----', cleans)
 
+# Екземпляр класу Note
 cleeps = Note('лягти спати', 'поставити будильник на 22 год і одразу лягти спати')
 cleeps.add_teg('обовязкове')
 cleeps.add_teg('щоденне')
 
 notebook.add_note(cleeps)
 
-print('_____', cleeps)
+print('------------------------------------------')
 
 
-for x, y in notebook.data.items():
-    print(x, '_-_', y)
+# Зміна тексту нотатки
+cleans.change_note('викликати клінінговий сервіс')
+print(cleans)
+
+print('------------------------------------------')
+
+# Пошук нотатки по назві
+print(notebook.find_note('лягти спати'))
+
+print('------------------------------------------')
+
+# Пошук по тегам
+print(notebook.find_note_teg('обовязкове'))
+
+for i in notebook.find_note_teg('щоденне'):
+    print(i)
+
+print('------------------------------------------')
+
